a46e81db12668678dd6a7ba0a665793e68232901
sikong <sikong@geometricalpal.com>
2025-08-07 02:46:54 +0000

    辅助变动: 调试airflow1

--- train.sh
+++ train.sh
@@ -1,6 +1,9 @@
 current_time=$(date +%Y%m%d_%H_%M_%S)
 echo $current_time
 
+echo "[INSTALL ENVS]:"
+pip install pandas
+
 echo "[READ GLOBAL ENV VAR]:"
 airflow_key="gpal_neural_network_one_node_traning_job_on_airflow"
 echo "AIRFLOW_CTX_DAG_ID=$AIRFLOW_CTX_DAG_ID"
@@ -13,7 +16,7 @@ export ENV_GPAL_NEURAL_NETWORK_WORKSPACE_ROOT=$ENV_GPAL_NEURAL_NETWORK_AIRFLOW_W
 export ENV_GPAL_NEURAL_NETWORK_DATASETS_ROOT='/opt/airflow/datasets/'
 export ENV_GPAL_NEURAL_NETWORK_DATA_COLLECT_ROOT='/opt/airflow/process-prod-bucket/data_collect'
 export ENV_GPAL_NEURAL_NETWORK_LOCAL_DATASETS_ROOT='/opt/airflow/local_datasets/'
-export WORLD_SIZE=2
+export WORLD_SIZE=1
 worspace=$ENV_GPAL_NEURAL_NETWORK_WORKSPACE_ROOT/${AIRFLOW_CTX_DAG_ID}_${current_time}
 gpus=8
 
