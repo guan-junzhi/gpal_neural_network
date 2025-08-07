8fab483861461d9777b575ee74d5cb8e7d5b14f0
sikong <sikong@geometricalpal.com>
2025-08-07 11:15:58 +0000

    Revert "辅助变动: 调试airflow4"
    This reverts commit 286b7762a2e84fb628d11def6a45ab07fe0eb2f1.

--- gpal_lightning/utils/profiling.py
+++ gpal_lightning/utils/profiling.py
@@ -65,7 +65,6 @@ class DetailProf:
 
 def GetMemInfo():
     a = os.popen("free -m").readlines()
-    print(a)
     titles = [ele for ele in a[0].replace('\n', '').split(' ') if ele != '']
     values = [ele for ele in a[1].replace(
         '\n', '').split(' ') if ele != ''][1:]
