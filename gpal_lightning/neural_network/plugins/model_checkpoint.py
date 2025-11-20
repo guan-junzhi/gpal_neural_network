04248ec8ec90ead3a12dac8b4831157aa52edefb
huiquyang <huiquyang@geometricalpal.com>
2025-11-20 04:00:18 +0000

    问题修复：bugfix

--- gpal_lightning/neural_network/plugins/model_checkpoint.py
+++ gpal_lightning/neural_network/plugins/model_checkpoint.py
@@ -20,8 +20,10 @@ def _search_prefix(dict_to_search, prefix):
     return dict_after_search
 
 def Simplify(dict_to_search):
-    # while len(set([k.split('.')[0] for k in dict_to_search])) == 1:
-    #     dict_to_search = {'.'.join(k.split('.')[1:]): dict_to_search[k] for k in dict_to_search}
+    while len(set([k.split('.')[0] for k in dict_to_search])) == 1:
+        if 'conv_out' in dict_to_search:
+            break
+        dict_to_search = {'.'.join(k.split('.')[1:]): dict_to_search[k] for k in dict_to_search}
     return dict_to_search
 
 
