bdd1a4e4e1d919bdc2ba55187ad7fb06b89dac92
huiquyang <huiquyang@geometricalpal.com>
2026-01-16 03:25:17 +0000

    辅助变动：数据挖掘重新挖掘

--- gpal_lightning/utils/data_buffer.py
+++ gpal_lightning/utils/data_buffer.py
@@ -9,7 +9,7 @@ import lmdb
 class FastLoaderBuffer():
     def __init__(self, filename) -> None:
         self.lmdb_path = filename
-        self.map_size = 1099511627776 * 5
+        self.map_size = 1099511627776 * 6
         self.lmdb = None
 
     def Cache(self, key, data):
